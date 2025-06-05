e8825b5218c50ac9997ed51425b59c52b6e4f725
ericsondalay <eicsondalay@gmail.com>
2025-06-05 14:06:06 +0800

    modified dashboard

--- src/pages/MissionPage.jsx
+++ src/pages/MissionPage.jsx
@@ -4,7 +4,6 @@ import MissionList from "../components/dashboard";
 export default function MissionsPage() {
   return (
     <div className="py-4">
-      <h2 className="text-center mb-4">Mission List</h2>
       <MissionList />
     </div>
   );
